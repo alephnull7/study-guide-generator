4ad775faa129c00367ab31aa6db9e3e4168e8f74
Xander Renkema <105459386+RedTechGamer236@users.noreply.github.com>
2024-04-03 17:33:24 -0500

    Add scrolling styles

--- views/styles.js
+++ views/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexGrow: 1,
       justifyContent: 'flex-start',
       alignItems: 'center',
       padding: 20,
