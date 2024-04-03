b09216676d87cdf9937ea6cda4cbce34f18ce336
Xander Renkema <105459386+RedTechGamer236@users.noreply.github.com>
2024-04-03 16:17:34 -0500

    Add Account and Logout support

--- views/instructorHomeView.js
+++ views/instructorHomeView.js
@@ -1,14 +1,24 @@
 import * as React from 'react';
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { View, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 
 const InstructorHomeView = () => {
     const navigation = useNavigation();
+    const route = useRoute();
+
+    const { username } = route.params;
+
+    const handleLogout = () => {
+      navigation.reset({
+        index: 0,
+        routes: [{name: "Welcome"}]
+      })
+    }
   
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>User Account</Text>
+        <Text style={styles.header}>Welcome, {username}</Text>
         <TouchableOpacity 
           style={styles.button}
           onPress={() => navigation.navigate('Study Guides')}
@@ -32,6 +42,9 @@ const InstructorHomeView = () => {
           onPress={() => navigation.navigate('Manage Students')}
         >
           <Text style={styles.buttonText}>Manage Students</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
         </TouchableOpacity>
       </View>
     );
