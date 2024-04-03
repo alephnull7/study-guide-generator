9e9eabaf6eb1244f781872016e6f1fc75fe6bede
Xander Renkema <105459386+RedTechGamer236@users.noreply.github.com>
2024-04-03 15:00:50 -0500

    Add login support

--- views/loginView.js
+++ views/loginView.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginView = () => {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
+    const navigation = useNavigation();
+
+    const handleLogin = () => {
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Instructor Home'}]
+      });
+    }
   
     return (
       <View style={styles.container}>
@@ -26,7 +35,7 @@ const LoginView = () => {
           secureTextEntry={true}
           autoCompleteType="password"
         />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
       </View>
