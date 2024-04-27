49abb741b05da45d08221fe2f6c6de8ae43c45d6
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-04-27 10:50:34 -0700

    Add artifacts to classroom view

--- views/viewClassroomView.js
+++ views/viewClassroomView.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
-import {Text, TouchableOpacity, View} from "react-native";
+import {ScrollView, Text, TouchableOpacity, View} from "react-native";
 import styles from "../styles/styles";
 import { fetchDataFromAPI } from '../helpers/helpers';
 import { useAuth } from "../contexts/authContext";
+import {useNavigation} from "@react-navigation/native";
 
 const ViewClassroomView = ({ route }) => {
     const { authData } = useAuth();
+    const navigation = useNavigation();
 
     const [classroom, setClassroom] = useState({});
     const [students, setStudents]= useState({});
@@ -22,6 +24,12 @@ const ViewClassroomView = ({ route }) => {
     useEffect(() => {
         if (Object.keys(classroom).length !== 0) {
             fetchAndSetStudents();
+        }
+    }, [classroom]);
+
+    useEffect(() => {
+        if (Object.keys(classroom).length !== 0) {
+            fetchAndSetArtifacts();
         }
     }, [classroom]);
 
@@ -47,19 +55,62 @@ const ViewClassroomView = ({ route }) => {
         }
     };
 
+    const fetchAndSetArtifacts = async () => {
+        try {
+            const response = await fetchDataFromAPI(`classrooms/artifacts/${classroom.id}`, authData.token);
+            switch (response.status) {
+                case 204:
+                    setErrorText('');
+                    setSuccessText('The classroom has no artifacts.');
+                    return;
+                case 200:
+                    setErrorText('');
+                    console.log(response.body);
+                    setArtifacts(response.body);
+                    return;
+                default:
+                    throw new Error("Unsuccessful retrieval of artifacts");
+            }
+        } catch (error) {
+            console.error(`Error getting classroom's artifacts:`, error.message);
+            setErrorText(`Unable to access artifacts.`);
+        }
+    };
+
     return(
         <View style={styles.container}>
             <Text style={styles.header}>{classroom.name}</Text>
-            <View>
+            <ScrollView>
                 <Text style={styles.header}>Students</Text>
-                {Object.values(students).map(student => (
-                    <Text
-                        key={student.uid}
-                        style={styles.button}>
-                        {student.username}
-                    </Text>
-                ))}
-            </View>
+                    {Object.values(students).map(student => (
+                        <Text
+                            key={student.uid}
+                            style={styles.button}>
+                            {student.username}
+                        </Text>
+                    ))}
+                <Text style={styles.header}>Artifacts</Text>
+                    {Object.values(artifacts).map(artifact => (
+                        <TouchableOpacity
+                            key={artifact.id}
+                            style={styles.button}
+                            onPress={() => navigation.navigate('Artifact', { artifact: artifact })}>
+                            <Text style={styles.buttonText}>
+                                {artifact.name}
+                                {"\n"}
+                                {artifact.code}
+                                {"\n"}
+                                {artifact.course}
+                            </Text>
+                        </TouchableOpacity>
+                    ))}
+            </ScrollView>
+            {errorText !== '' && (
+                <Text style={styles.errorText}>{errorText}</Text>
+            )}
+            {successText !== '' && (
+                <Text style={styles.successText}>{successText}</Text>
+            )}
         </View>
     );
 };
