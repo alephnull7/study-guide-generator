cb164dc68f18977d2c52e3332ae1b4e849e6b274
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-05-01 21:30:24 -0700

    Implementation of PDF export for web

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,3 +1,6 @@
+import {Platform} from "react-native";
+import * as FileSystem from "expo-file-system";
+
 const base_url = 'https://yhh2mupveh.us-east-2.awsapprunner.com/api';
 export const fetchDataFromAPI = async (route, authContext, retry = true) => {
     try {
@@ -56,6 +59,51 @@ export const sendDataToAPI = async (route, method, data, authContext, retry = tr
         };
     } catch (error) {
         console.log('Error sending data:', error.message);
+        return {
+            status: 500,
+            body: {}
+        };
+    }
+};
+
+export const fetchAndSavePDFFromAPI = async (route, authContext, fileName, retry = true) => {
+    try {
+        const { authData } = authContext;
+        const url = `${base_url}/${route}`;
+
+        if (Platform.OS === 'web') {
+            const response = await fetch(url, {
+                headers: {
+                    'Accept': `application/pdf`,
+                    'Authorization': `Bearer ${authData?.token}`
+                }
+            });
+            const blob = await response.blob();
+            const downloadUrl = window.URL.createObjectURL(blob);
+
+            // Create a link element to trigger the download
+            const link = document.createElement('a');
+            link.href = downloadUrl;
+            link.setAttribute('download', `${fileName}.pdf` || 'example.pdf');
+            link.style.display = 'none';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            console.log('This might work for mobile?');
+            const headers = {
+                Authorization: `Bearer ${authContext.authData}`
+            };
+            const fileInfo = await FileSystem.downloadAsync(url,
+              FileSystem.documentDirectory, {headers});
+            const savedFile = FileSystem.documentDirectory + `${fileName}.pdf`;
+            await FileSystem.moveAsync({
+                from: fileInfo.uri,
+                to: savedFile,
+            });
+        }
+    } catch (error) {
+        console.error('Error getting PDF:', error.message);
         return {
             status: 500,
             body: {}
